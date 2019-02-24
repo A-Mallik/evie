5f656501f1517ca869f3b5ae5a46cc8a78ecf252
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-02-24 10:28:20 -0500

    error in sentiment

---
+++ src/content.js
@@ -0,0 +1,25 @@
+var articleArray = [];
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+window.onload = function () {
+    var url = window.location.href;
+    alert("This is the url " + url);
+    var x = document.querySelectorAll("p");
+    x.forEach((node) => {
+        articleArray.push(node.innerText);
+    });
+   console.log(articleArray[0]);
+    // var story = articleArray.join(", ");
+    // console.log("Here is the story " + story);
+}
+
+// var story = articleArray.join(", ");
+
+// console.log(articleArray)
+
+// var Sentiment = require('sentiment');
+// var sentiment = new Sentiment();
+
+// var result = sentiment.analyze(story);
+
+// console.dir(result);
---
+++ src/sentiment.js
@@ -0,0 +1,6 @@
+var content = require("./content")
+var story = content.story
+
+// import story from './content.js'
+
+console.log("Here is the story: " + story)
