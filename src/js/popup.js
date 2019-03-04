287df68b2be7346ae6bc7bc9f19af56e7f57ea11
Shohan <ashraful.mallik100@gmail.com>
2019-03-03 23:13:19 -0500

    Update to manifest.json

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,37 +1,66 @@
-window.onload = () => {
-  const $startButton = document.querySelector('.start');
+// import 'sentiment.js'
 
-  $startButton.onclick = () => {
-    // Get active tab
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    }, (tabs) => {
-      // Send message to script file
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        { injectApp: true },
-        response => window.close()
-      );
+var articleArray = [];
+// var Sentiment = require('sentiment');
+// var sentiment = new Sentiment();
+window.onload = function () {
+    var url = window.location.href;
+    alert("This is the url " + url);
+    var x = document.querySelectorAll("p");
+    x.forEach((node) => {
+        articleArray.push(node.innerText);
     });
-  };
-
-  var articleArray = [];
-  // var Sentiment = require('sentiment');
-  // var sentiment = new Sentiment();
-
-      var url = window.location.href;
-      alert("This is the url " + url);
-      var x = document.querySelectorAll("p");
-      x.forEach((node) => {
-          articleArray.push(node.innerText);
-      });
-    console.log(articleArray);
-      // var story = articleArray.join(", ");
-      // console.log("Here is the story " + story);
-  
-  
-
-
-
+   console.log(articleArray);
+    // var story = articleArray.join(", ");
+    // console.log("Here is the story " + story);
 }
+
+// var story = articleArray.join(", ");
+
+// console.log(articleArray)
+
+// var Sentiment = require('sentiment');
+// var sentiment = new Sentiment();
+
+// var result = sentiment.analyze(story);
+
+// console.dir(result);
+
+
+// window.onload = () => {
+//   // const $startButton = document.querySelector('.start');
+
+//   // $startButton.onclick = () => {
+//   //   // Get active tab
+//   //   chrome.tabs.query({
+//   //     active: true,
+//   //     currentWindow: true,
+//   //   }, (tabs) => {
+//   //     // Send message to script file
+//   //     chrome.tabs.sendMessage(
+//   //       tabs[0].id,
+//   //       { injectApp: true },
+//   //       response => window.close()
+//   //     );
+//   //   });
+//   // };
+
+//   var articleArray = [];
+//   // var Sentiment = require('sentiment');
+//   // var sentiment = new Sentiment();
+
+//       var url = window.location.href;
+//       alert("This is the url " + url);
+//       var x = document.querySelectorAll("p");
+//       x.forEach((node) => {
+//           articleArray.push(node.innerText);
+//       });
+//     console.log(articleArray);
+//       // var story = articleArray.join(", ");
+//       // console.log("Here is the story " + story);
+  
+  
+
+
+
+// }
